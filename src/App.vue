ec8279f48302ebdb6cf6d809aa15f523f650589a
TheJustRusik <7thejustrusik7@gmail.com>
2023-11-28 11:11:18 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+// import HelloWorld from './components/Component1.vue'
+// import Btn from './components/Button.vue'
+</script>
+
+<template>
+    <h1 class="text-red-700">Hello world!</h1>
+</template>
